9dc5784b3e8e5286cbc069127382f6ea36c58fe8
Tommi Seppänen <tommi.t.seppanen@gmail.com>
2019-04-20 15:56:57 +0300

    Display current day

--- app/src/main/java/com/tomtase/ninetofivewearos/MainActivity.kt
+++ app/src/main/java/com/tomtase/ninetofivewearos/MainActivity.kt
@@ -2,6 +2,10 @@ package com.tomtase.ninetofivewearos
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.util.*
 
 class MainActivity : WearableActivity() {
 
@@ -11,5 +15,12 @@ class MainActivity : WearableActivity() {
 
         // Enables Always-on
         setAmbientEnabled()
+
+        val time = SimpleDateFormat("EEE d.M.").format(Date())
+        weekday.text = time
+    }
+
+    fun setTargetTimes(startTime: Date) {
+        val currentMinutes = Date().time - startTime.time
     }
 }
